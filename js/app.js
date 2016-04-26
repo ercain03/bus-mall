77f86766bb982c996b023900f3ab61274cb02533
ercain03 <ercain3@gmail.com>
2016-04-26 15:12:20 -0700

    added chart and fixed app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
 var imageArr = [];
-var randomNumGenArr = [];
+var randomGenNumbs = [];
 var clickNums = 0;
-var firstPic = document.getElementById('image-first');
-var secondPic = document.getElementById('image-second');
-var thirdPic = document.getElementById('image-third');
+var firstPic = document.getElementById('imageFirst');
+var secondPic = document.getElementById('imageSecond');
+var thirdPic = document.getElementById('imageThird');
 
 function randomNumGen(min,max){
-  var rando = Math.floor(Math.random() * (max - min)) + min;
-  return rando;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function imageOption(name, type, path,numTimesDisplayed,numTimesClicked){
+function imageOption(name, type){
   this.name = name;
   this.type = type;
   this.path = 'images/' + this.name + this.type;
-  this.imageSeen = imageSeen;
-  this.userClicks = userClicks;
+  this.imageSeen = 0;
+  this.userClicks = 0;
   imageArr.push(this);
 }
 
@@ -36,6 +35,106 @@ new imageOption('shark', '.jpg');
 new imageOption('sweep', '.png');
 new imageOption('tauntaun', '.jpg');
 new imageOption('unicorn', '.jpg');
-new imageOption('usb', '.jpg');
+new imageOption('usb', '.gif');
 new imageOption('water-can', '.jpg');
 new imageOption('wine-glass', '.jpg');
+
+function randomNumGenerator(){
+  for(var i = 0; i < 3; i++){
+    randomGenNumbs.push(randomNumGen(0, imageArr.length));
+  }
+  while (randomGenNumbs[1] === randomGenNumbs[0]) {
+    randomGenNumbs[1] = randomNumGen(0, imageArr.length);
+  }
+  while (randomGenNumbs[2] === randomGenNumbs[1] || randomGenNumbs[2] === randomGenNumbs[0]) {
+    randomGenNumbs[2] = randomNumGen(0, imageArr.length);
+  }
+}
+
+var firstPic = document.getElementById('imageFirst');
+var secondPic = document.getElementById('imageSecond');
+var thirdPic = document.getElementById('imageThird');
+
+function imageRender(){
+  randomNumGenerator();
+  firstPic.src = imageArr[randomGenNumbs[0]].path;
+  firstPic.id = imageArr[randomGenNumbs[0]].name;
+  secondPic.src = imageArr[randomGenNumbs[1]].path;
+  secondPic.id = imageArr[randomGenNumbs[1]].name;
+  thirdPic.src = imageArr[randomGenNumbs[2]].path;
+  thirdPic.id = imageArr[randomGenNumbs[2]].name;
+  imageArr[randomGenNumbs[0]].imageSeen++;
+  imageArr[randomGenNumbs[1]].imageSeen++;
+  imageArr[randomGenNumbs[2]].imageSeen++;
+  randomGenNumbs = [];
+}
+imageRender();
+
+function clickHandler(event){
+  var targetEl = event.target;
+  if(clickNums === 25){
+    makeChart();
+  }
+    for ( var i = 0; i < imageArr.length ; i++){
+      if (imageArr[i].name === event.target.id){
+        imageArr[i].userClicks++;
+        clickNums ++;
+        imageRender();
+        break;
+      }
+    }
+  }
+
+function makeChart(){
+  var names = [];
+  var percents = [];
+  for (var i = 0; i < imageArr.length; i++){
+    names.push(imageArr[i].name);
+    percents.push(imageArr[i].userClicks);
+  }
+
+  var data = {
+    labels: names,
+    datasets: [
+      {
+        data: percents,
+        backgroundColor: [
+          'red',
+          'orange',
+          'yellow',
+          'green',
+          'blue',
+          'indigo',
+          'violet',
+          '#790e08',
+          '#7d443d',
+          '#89a5bb',
+          '#ab831b',
+          '#ead83c',
+          '#22d7a5',
+          '#760567',
+          '#52f6bf',
+          '#30a702',
+          '#534317',
+          '#cf7e0b',
+          '#3017ec',
+          '#4f7c9a'
+        ],
+        hoverBackgroundColor: [
+
+        ]
+      }]
+  };
+
+  var chosenAns = document.getElementById('cust-chart').getContext('2d');
+  var myChart = new Chart(chosenAns, {
+    type: 'pie',
+    data: data,
+    options: {
+      responsive: false
+    }
+  });
+}
+
+var elClicko = document.getElementById('displated-images');
+document.addEventListener('click', clickHandler);
